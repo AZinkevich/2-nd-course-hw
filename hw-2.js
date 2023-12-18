d4df09f052af27901ed61795d9ca9eafe29b3581
AZinkevich <azinkevich@mail.ru>
2023-12-18 23:43:49 +1100

    добавил hw-2.js

---
+++ hw-2.js
@@ -0,0 +1,15 @@
+//task 1
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+
+//task 2
+const yearIphone1 = 2007;
+alert(`Первый Iphone был выпущен в ${yearIphone1}`);
+
+//task 3
+const JSCreator = "Брендан Эйх";
+alert(`${JSCreator} - создатель языка JavaScript`);
+
+//task4
