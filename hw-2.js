30800fffac039c1b98306d0eb2b8b2c779634629
AZinkevich <AZinkevich@mail.ru>
2023-12-19 23:32:01 +1100

    исправил некритичные синтаксические ошибки

--- hw-2.js
+++ hw-2.js
@@ -15,55 +15,55 @@ alert(`${JSCreator} - создатель языка JavaScript`);
 //task4
 let a4 = 10;
 let b4 = 2;
-let sum4 = a4+b4;
-let dif4 = a4-b4;
-let prod4 = a4*b4;
-let quot4 = a4/b4;
+let sum4 = a4 + b4;
+let dif4 = a4 - b4;
+let prod4 = a4 * b4;
+let quot4 = a4 / b4;
 alert(`${sum4}, ${dif4}, ${prod4}, ${quot4}`);
 
 //task5
-let result5 = 2**5;
+let result5 = 2 ** 5;
 alert(result5);
 
 //task6
 let a6 = 9;
 let b6 = 2;
-alert(a6%b6);
+alert(a6 % b6);
 
 //task7
 let num = 1;
-num +=5;
-num -=3;
-num *=7;
-num /=3;
-num =++num;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num = ++num;
 num = --num;
 alert(num);
 
 //task8
-let age = Number(prompt('Сколько вам лет?'));
+let age = Number(prompt("Сколько вам лет?"));
 alert(age);
 
 //task9.0
 const user = {
-    name: 'Alex',
-    age: 47,
-    isAdmin: true
-}
+  name: "Alex",
+  age: 47,
+  isAdmin: true,
+};
 
 //task9.1
-user['city of residence'] = false;
+user["city of residence"] = false;
 
 //task9.2
 user.age = 48;
 
 //task9.3
-delete user['city of residence'];
+delete user["city of residence"];
 
 //task9.4
 let info = prompt(`Какую информацию хотите узнать о пользователе?`);
 alert(user[info]);
 
 //task10
-let name10  = prompt('Введи свое имя');
+let name10 = prompt("Введи свое имя");
 alert(`Привет, ${name10}!`);
