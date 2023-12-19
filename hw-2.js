e890bcddcf00691df111960b379601ebca6106e3
AZinkevich <AZinkevich@mail.ru>
2023-12-19 11:25:26 +1100

    task9

--- hw-2.js
+++ hw-2.js
@@ -13,3 +13,37 @@ const JSCreator = "Брендан Эйх";
 alert(`${JSCreator} - создатель языка JavaScript`);
 
 //task4
+let a4 = 10;
+let b4 = 2;
+let sum4 = a4+b4;
+let dif4 = a4-b4;
+let prod4 = a4*b4;
+let quot4 = a4/b4;
+alert(`${sum4}, ${dif4}, ${prod4}, ${quot4}`);
+
+//task5
+let result5 = 2**5;
+alert(result5);
+
+//task6
+let a6 = 9;
+let b6 = 2;
+alert(a6%b6);
+
+//task7
+let num = 1;
+num +=5;
+num -=3;
+num *=7;
+num /=3;
+num =++num;
+num = --num;
+alert(num);
+
+//task8
+let age = Number(prompt('Сколько вам лет?'));
+alert(age);
+
+//task9.0
+let user {name: Alex;
+}
