45de747c9f2908a48455a486c5e78fc9fbad1eda
AZinkevich <AZinkevich@mail.ru>
2023-12-23 00:18:01 +1100

    задание 9

--- hw-3.js
+++ hw-3.js
@@ -1,60 +1,62 @@
 //task1
-// let password = "qwerty";
-// let userPassword = prompt(`Веведите пароль`);
-// if (password === userPassword) {
-//   alert("Пароль введен верно");
-// } else {
-//   alert("Пароль введен неправильно");
-// }
+let password = "qwerty";
+let userPassword = prompt(`Веведите пароль`);
+if (password === userPassword) {
+  alert("Пароль введен верно");
+} else {
+  alert("Пароль введен неправильно");
+}
 
 //task2
-// let c = -11;
-// c > 0 && c < 10 ? console.log("Верно") : console.log("Неверно");
+let c = -11;
+c > 0 && c < 10 ? console.log("Верно") : console.log("Неверно");
 
 //task3
-// let d = 10;
-// let e = 10;
-// d > 100 || e > 100 ? console.log("Верно") : console.log("Неверно");
+let d = 10;
+let e = 10;
+d > 100 || e > 100 ? console.log("Верно") : console.log("Неверно");
 
 //task4
-// let a = '2';
-// let b = '3';
-// alert(Number(a) + Number(b));
+let a = '2';
+let b = '3';
+alert(Number(a) + Number(b));
 
 //task5
-// let monthNumber = prompt("Чтобы узнать время года укажи номер месяца:");
-// switch (monthNumber) {
-//   case "1":
-//   case "2":
-//   case "12":
-//     console.log("Зима");
-//     break;
-//   case "3":
-//   case "4":
-//   case "5":
-//     console.log("Весна");
-//     break;
-//   case "6":
-//   case "7":
-//   case "8":
-//     console.log("Лето");
-//     break;
-//   case "9":
-//   case "10":
-//   case "11":
-//     console.log("Осень");
-//     break;
-//   default:
-//     console.log("Введи число от 1 до 12!");
-//     break;
-// }
+let monthNumber = prompt("Чтобы узнать время года укажи номер месяца:");
+switch (monthNumber) {
+  case "1":
+  case "2":
+  case "12":
+    console.log("Зима");
+    break;
+  case "3":
+  case "4":
+  case "5":
+    console.log("Весна");
+    break;
+  case "6":
+  case "7":
+  case "8":
+    console.log("Лето");
+    break;
+  case "9":
+  case "10":
+  case "11":
+    console.log("Осень");
+    break;
+  default:
+    console.log("Введи число от 1 до 12!");
+    break;
+}
 
 //task7
 let var7 = Number(prompt(`Пожалуйста, введите любое число`));
-if (Number.isNaN(var7)) {
+if (isNaN(var7)) {
   console.log("Это не число!");
 } else if (var7 % 2 > 0) {
   console.log("Это число нечетное");
 } else {
   console.log("Это число четное");
 }
+
+//task8
