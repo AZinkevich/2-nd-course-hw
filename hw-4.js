c00f49812c74a18154da6ea05302afda3297f09d
AZinkevich <azinkevich@mail.ru>
2023-12-25 23:53:13 +1100

    примеры

---
+++ hw-4.js
@@ -0,0 +1,24 @@
+// while (true) {
+//     const msg = prompt("введите сообщение");
+//     if (msg === 'continue') {
+//         continue;
+//     }
+//     console.log(msg);
+//     if (msg === 'end') {
+//         break;
+//     }
+// }
+// let monthNumber = 1;
+
+// while (monthNumber <= 12) {
+//     if (monthNumber >= 1 && monthNumber <=2 || monthNumber === 12) {
+//         console.log('Зима');
+//     } else if (monthNumber >= 3 && monthNumber <= 5) {
+//         console.log('Весна');
+//     } else if (monthNumber >= 6 && monthNumber <= 8) {
+//         console.log('Лето');
+//     } else if (monthNumber >= 9 && monthNumber <= 11) {
+//         console.log('Осень');
+//     }
+// 	monthNumber++; // Увеличиваем значение monthNumber для следующего шага на 1
+// }
