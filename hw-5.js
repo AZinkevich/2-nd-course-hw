ada5eee8e30133cbd76d6c5ec8937fa88c6219a3
AZinkevich <AZinkevich@mail.ru>
2023-12-29 16:09:49 +1100

    task2

--- hw-5.js
+++ hw-5.js
@@ -1,26 +1,49 @@
-function area() {
-  return this.w * this.h;
-}
+// function area() {
+//   return this.w * this.h;
+// }
 
-function perimeter() {
-  return (this.w + this.h) * 2;
-}
+// function perimeter() {
+//   return (this.w + this.h) * 2;
+// }
 
-const square1 = {
-  w: 20,
-  h: 30,
-  a: area,
-  p: perimeter,
-};
+// const square1 = {
+//   w: 20,
+//   h: 30,
+//   a: area,
+//   p: perimeter,
+// };
 
-const square2 = {
-  w: 10,
-  h: 50,
-  a: area,
-  p: perimeter,
-};
+// const square2 = {
+//   w: 10,
+//   h: 50,
+//   a: area,
+//   p: perimeter,
+// };
 
-console.log(square1.a());
-console.log(square2.a());
-console.log(square1.p());
-console.log(square2.p());
+// console.log(square1.a());
+// console.log(square2.a());
+// console.log(square1.p());
+// console.log(square2.p());
+
+//task1
+// function min() {
+//   if (a < b) {
+//     return a;
+//   } else if (a > b) {
+//     return b;
+//   } else {
+//     return a;
+//   }
+// }
+
+//task2
+// function parity() {
+//   let n = Number(prompt("Введите число"));
+//   if (n % 2 == 0) {
+//     return console.log("Число четное");
+//   } else {
+//     return console.log("Число нечетное");
+//   }
+// }
+
+//task3
