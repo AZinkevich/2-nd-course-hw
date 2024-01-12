e8a53bb52ab501d29ba1929182f0b4e87f3247ad
AZinkevich <AZinkevich@mail.ru>
2024-01-12 18:00:19 +1100

    task7

---
+++ hw-6.js
@@ -0,0 +1,48 @@
+//task1
+// const arr1 = [1, 5, 4, 10, 0, 3];
+// for (let index = 0; index < arr1.length; index++) {
+//     console.log(arr1[index]);
+//     if (arr1[index] == 10) break;
+// }
+
+//task2
+// const arr2 = [1, 5, 4, 10, 0, 3];
+// let posItem2 = arr2.indexOf(4);
+// console.log(posItem2);
+
+//task3
+// let arr3 = [1, 3, 5, 10, 20];
+// arr3 = arr3.join(' ');
+// console.log(arr3);
+
+//task4
+// let arr4 = [];
+// for (let i = 0; i < 3; i++) {
+//   arr4[i] = [];
+//   for (let k = 0; k < 3; k++) {
+//     arr4[i][k] = 1;
+//   }
+// }
+// console.log(arr4);
+
+//task5
+// const arr5 = [1, 1, 1];
+// for (let h = 0; h < 3; h++) {
+//     arr5.push(2);
+// }
+// console.log(arr5);
+
+//task6
+// let arr6 = [9, 8, 7, "a", 6, 5];
+// arr6.sort();
+// arr6.pop();
+// console.log(arr6);
+
+//task7
+const arr7 = [9, 8, 7, 6, 5];
+let search = arr7.includes(Number(prompt("Введите число")));
+if (search == 1) {
+  alert("Угадал");
+} else {
+  alert("Не угадал");
+}
