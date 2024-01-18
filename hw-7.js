5d9b10ddd14ec2667b844035b07c3e4850176302
AZinkevich <AZinkevich@mail.ru>
2024-01-19 10:40:56 +1100

    task4

---
+++ hw-7.js
@@ -0,0 +1,31 @@
+//task1
+// let str = "AbCdEf";
+// str = str.toUpperCase();
+// console.log(str);
+
+//task2
+// function searchStart(arr, str) {
+//   let newArr = [];
+//   arr.forEach((substr) => {
+//     if (substr.toLowerCase().startsWith(str.toLowerCase())) {
+//       newArr.push(substr);
+//     }
+//   });
+//   return newArr;
+// }
+
+// console.log(searchStart(["Кошка", "Кит", "Комар", "Носорог"], "ко"),
+// searchStart(["яблоко", "груша", "гриб", "огурец"], "гру"),
+// searchStart(["Дом", "Банк", "Больница", "Театр"], "Кино"));
+
+//task3
+// const int3 = 32.58884;
+// console.log(Math.floor(int3), Math.ceil(int3), Math.round(int3));
+
+//task4
+// function getRandomInt(minValue, maxValue) {
+//   return Math.round(Math.random() * (maxValue - minValue)) + minValue;
+// }
+// console.log(getRandomInt(1, 10));
+
+//task5
