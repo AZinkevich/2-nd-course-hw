2926b3f771b9d4e0f74dc0d6a43ab6a5bec82ab6
AZinkevich <AZinkevich@mail.ru>
2024-01-19 19:05:12 +1100

    task9

--- hw-7.js
+++ hw-7.js
@@ -23,13 +23,6 @@
 // console.log(Math.floor(int3), Math.ceil(int3), Math.round(int3));
 
 //task4
-// function getRandomInt(minValue, maxValue) {
-//   return Math.round(Math.random() * (maxValue - minValue)) + minValue;
-// }
-// console.log(getRandomInt(1, 10));
-
-//task5
-
 // console.log(Math.min(52, 53, 49, 77, 21, 32));
 // console.log(Math.max(52, 53, 49, 77, 21, 32));
 
@@ -42,12 +35,45 @@
 // console.log(Math.min.apply(null, newArr5));
 // console.log(Math.max.apply(null, newArr5));
 
+//task5
+// function getRandomInt(minValue, maxValue) {
+//   return Math.random() * (maxValue - minValue) + minValue;
+// }
+// console.log(getRandomInt(1, 10));
+
 //task6
-function getRandomArrNumbers(maxValue) {
-  let randomArr = [];
-  for (let i = 0; i < Math.floor(maxValue / 2); i++) {
-    randomArr.push(Math.round(Math.random() * maxValue));
-  }
-  return randomArr;
-}
-console.log(getRandomArrNumbers(7), getRandomArrNumbers(12));
+// function getRandomArrNumbers(maxValue) {
+//   let randomArr = [];
+//   for (let i = 0; i < Math.floor(maxValue / 2); i++) {
+//     randomArr.push(Math.round(Math.random() * maxValue));
+//   }
+//   return randomArr;
+// }
+// console.log(getRandomArrNumbers(7), getRandomArrNumbers(12));
+
+//task7
+// function getRandomInt7(minValue, maxValue) {
+//   return Math.round(Math.random() * (maxValue - minValue)) + minValue;
+// }
+
+// console.log(getRandomInt7(3, 5));
+
+//task8
+// let currentDate8 = new Date();
+// console.log(currentDate8);
+
+//task9
+// let currentDate9 = new Date();
+// console.log(+currentDate9);
+// let days73 = 73 * 24 * 60 * 60 * 1000;
+// console.log(days73);
+// let searchDate = +currentDate9 + days73;
+// let dayAfter73 = new Date(searchDate);
+// console.log(dayAfter73);
+
+// let currentDate9 = new Date();
+// currentDate9.setDate(currentDate9.getDate() + 73);
+// console.log(currentDate9);
+
+//task10
+
