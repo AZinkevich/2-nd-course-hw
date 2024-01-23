0ca060e4e4949721f4b47122c0630fbb88cf3218
AZinkevich <AZinkevich@mail.ru>
2024-01-23 15:50:55 +1100

    task2

---
+++ hw-8.js
@@ -0,0 +1,51 @@
+//task1
+// const people = [
+//   { name: "Глеб", age: 29 },
+//   { name: "Анна", age: 17 },
+//   { name: "Олег", age: 7 },
+//   { name: "Оксана", age: 47 },
+// ];
+
+// console.log(people.sort(orderSort));
+
+// function orderSort(a, b) {
+//   if (a.age > b.age) {
+//     return 1;
+//   }
+//   if (a.age < b.age) {
+//     return -1;
+//   }
+//   return 0;
+// }
+
+// //task2
+// function isPositive(number) {
+//   return number >= 0;
+// }
+
+// function isMale(item) {
+//   return item.gender === "male";
+// }
+
+// function filter(arr, ruleFunction) {
+//   const outArr = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (ruleFunction(arr[i]) == true) {
+//       outArr.push(arr[i]);
+//     }
+//   }
+//   return outArr;
+// }
+
+// console.log(filter([3, -4, 1, 9], isPositive)); // Должен выводить [3, 1, 9]
+
+// const people = [
+//   { name: "Глеб", gender: "male" },
+//   { name: "Анна", gender: "female" },
+//   { name: "Олег", gender: "male" },
+//   { name: "Оксана", gender: "female" },
+// ];
+
+// console.log(filter(people, isMale)); // Должен выводить [{name: 'Глеб', gender: 'male'},  {name: 'Олег', gender: 'male'}]
+
+//task3
